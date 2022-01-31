208356529ef4679524af9d59718e7b4a03ac471a
Cintia Silva <cintia-b@live.com>
2022-01-30 21:46:45 -0300

    projeto cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,32 @@
 const cipher = {
-  // ...
-};
+  encode: function (numDeslocamento, MsgCod) {
+    let tamAlfabeto = 26;
+    //codigo Ascii é utilizado para codificar
+    let CodigoAscii = 65;
+    let Soma = "";
+    //i+ incremento desloca letras para + / -
+    for (let i = 0; i < MsgCod.length; i++) {
+      //charcode converte para string o numero do alf para letra
+      Soma += String.fromCharCode(
+        ((MsgCod.charCodeAt(i) - CodigoAscii + numDeslocamento) % tamAlfabeto) +
+          CodigoAscii
+      );      
+    }
+    return Soma;
+  },
+  decode: function (numDeslocamento, MsgCod) {
+    let tamAlfabeto = 26;
+    let CodigoAscii = 65;
+    let SomaDecode = "";
+    for (let i = 0; i < MsgCod.length; i++) {
+      SomaDecode += String.fromCharCode(
+        ((MsgCod.charCodeAt(i) - CodigoAscii - numDeslocamento) % tamAlfabeto) +
+          CodigoAscii
+      );
 
+      console.log(SomaDecode);
+    }
+    return SomaDecode;
+  },
+};
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-import cipher from './cipher.js';
+import cipher from "./cipher.js";
 
-console.log(cipher);
+const buttonCodificar = document.getElementById("codificar");
+buttonCodificar.addEventListener("click", function () {
+let MsgCod = document.getElementById("MsgCod").value;
+let offsetCod = document.getElementById("offsetCod").value;
+
+let encode = cipher.encode(Number(offsetCod), MsgCod);
+
+document.getElementById("codResultado").value = encode;
+});
+
+const buttonDecodificar = document.getElementById("btnDecodificar");
+buttonDecodificar.addEventListener("click", function () {
+let MsgDecodificar = document.getElementById("MsgDecodificar").value;
+let offsetDeco = document.getElementById("offsetDeco").value;
+
+let decode = cipher.decode(Number(offsetDeco), MsgDecodificar);
+document.getElementById("decoResultado").value = decode;
+});
