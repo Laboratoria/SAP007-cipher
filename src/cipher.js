990c626a00492c8d96cd013446c9b1c4f49e12de
Cintia Silva <cintia-b@live.com>
2022-02-03 22:17:21 -0300

    comentarios js

--- src/cipher.js
+++ src/cipher.js
@@ -2,12 +2,12 @@ const cipher = {
   encode: function (numDeslocamento, MsgCod) {
     let tamAlfabeto = 26;
     //codigo Ascii é utilizado para codificar
-    let CodigoAscii = 65;
+    let CodigoAscii = 65;//codigo de codificar
     let Soma = "";
     //i+ incremento desloca letras para + / -
-    for (let i = 0; i < MsgCod.length; i++) {
+    for (let i = 0; i < MsgCod.length; i++) { //incrementar/avançar uma casinha
       //charcode converte para string o numero do alf para letra
-      Soma += String.fromCharCode(
+      Soma += String.fromCharCode(//trazer o valor de numero para letras
         ((MsgCod.charCodeAt(i) - CodigoAscii + numDeslocamento) % tamAlfabeto) +
           CodigoAscii
       );      
