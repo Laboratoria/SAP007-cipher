16be37ed42c1b84eebd13d33cd88504b105bcb5f
Marione-Pereira <mari.tainara@gmail.com>
2022-02-10 14:54:04 -0300

    Primeira versão

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,75 @@
 const cipher = {
-  // ...
-};
+  encode: function (offset, string) {
+    if (offset == 0 || offset == null || offset == undefined) {
+      throw new TypeError ();
+   }
 
+
+    let mgsCode = "";
+
+    for (let i=0; i <string.length; i++) { //criando o loop
+    let encrypto = string[i].charCodeAt();
+    //pega as posições conforme a tabela
+    if (encrypto >= 65 && encrypto <= 90) {
+         mgsCode += String.fromCharCode(((encrypto - 65 + offset) % 26) + 65);
+      //para pegar as posições das letras maículas
+    }else if(encrypto >= 97 && encrypto <= 122) {
+        mgsCode += String.fromCharCode(((encrypto - 97 + offset) % 26) + 97);
+        //letras minúsculas
+    }else if (encrypto  >= 33 && encrypto <= 47) {//! " # $ % & ' ( ) * + ´ - etc
+      mgsCode += string.charAt(i);//devolve o prória caracter
+
+    }else if (encrypto  >= 58 && encrypto <= 64) {
+      mgsCode += string.charAt(i);
+
+    }else if (encrypto  >=91 && encrypto <= 96) {
+        mgsCode += string.charAt(i);
+
+    }else if(encrypto >= 123 && encrypto <= 254) {
+      mgsCode += string.charAt(i);
+  
+    }else if (encrypto == 32) {//caracter do espaço
+      mgsCode += string.charAt(i);//devolve o prória caracter
+
+  }
+  }
+ 
+
+    return mgsCode; //resultado 
+  },
+
+
+  decode: function (offset, string) {
+    if (offset == 0 || offset == null || offset == undefined) {
+       throw new TypeError ();
+    }
+
+  let mgsDecode = "";
+    for (let i=0; i <string.length; i++) {
+      let decrypto= string[i].charCodeAt();
+
+      if (decrypto >= 65 && decrypto <= 90) {
+        mgsDecode += String.fromCharCode(((decrypto + 65 - offset) % 26) + 65);
+
+    }else if (decrypto >= 97 && decrypto <= 122) {
+      mgsDecode += String.fromCharCode(((decrypto - 122 - offset) % 26) +122);
+
+    }else if(decrypto >= 33 && decrypto <=47 ) {
+      mgsDecode += string.charAt(i);
+
+    }else if(decrypto >= 58 && decrypto <=64 ) {
+      mgsDecode += string.charAt(i);
+    }else if(decrypto >= 91 && decrypto <=96 ) {
+         mgsDecode += string.charAt(i);        
+    }else if (decrypto >= 123 && decrypto <= 254 ) {
+      mgsDecode += string.charAt(i);
+
+    }else if (decrypto == 32) {
+      mgsDecode += string.charAt(i);
+
+}
+}
+    return mgsDecode;
+  }
+};
 export default cipher;
--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -39,9 +39,9 @@ describe('cipher', () => {
     // Se quiser adicionar testes para letras minúsculas, descomente o teste
     // abaixo.
     //
-    // it('should return "hijklmnopqrstuvwxyzabcdefg" for "abcdefghijklmnopqrstuvwxyz" with offset 33', () => {
-    //   expect(cipher.encode(33, 'abcdefghijklmnopqrstuvwxyz')).toBe('hijklmnopqrstuvwxyzabcdefg');
-    // });
+     it('should return "hijklmnopqrstuvwxyzabcdefg" for "abcdefghijklmnopqrstuvwxyz" with offset 33', () => {
+      expect(cipher.encode(33, 'abcdefghijklmnopqrstuvwxyz')).toBe('hijklmnopqrstuvwxyzabcdefg');
+     });
 
     // Hacker edition
     //
@@ -52,10 +52,10 @@ describe('cipher', () => {
     // [Português]
     // Se quiser adicionar testes para caracteres não alfabéticos, descomente o
     // teste abaixo.
-    //
-    // it('should return " !@" for " !@"', () => {
-    //   expect(cipher.encode(33, ' !@')).toBe(' !@');
-    // });
+    
+     it('should return " !@" for " !@"', () => {
+      expect(cipher.encode(33, ' !@')).toBe(' !@');
+     });
   });
 
   describe('cipher.decode', () => {
@@ -86,9 +86,9 @@ describe('cipher', () => {
     // Se quiser adicionar testes para letras minúsculas, descomente o teste
     // abaixo.
     //
-    // it('should return "abcdefghijklmnopqrstuvwxyz" for "hijklmnopqrstuvwxyzabcdefg" with offset 33', () => {
-    //   expect(cipher.decode(33, 'hijklmnopqrstuvwxyzabcdefg')).toBe('abcdefghijklmnopqrstuvwxyz');
-    // });
+     it('should return "abcdefghijklmnopqrstuvwxyz" for "hijklmnopqrstuvwxyzabcdefg" with offset 33', () => {
+      expect(cipher.decode(33, 'hijklmnopqrstuvwxyzabcdefg')).toBe('abcdefghijklmnopqrstuvwxyz');
+     });
 
     // Hacker edition
     //
@@ -99,10 +99,10 @@ describe('cipher', () => {
     // [Português]
     // Se quiser adicionar testes para caracteres não alfabéticos, descomente o
     // teste abaixo.
-    //
-    // it('should return " !@" para " !@"', () => {
-    //   expect(cipher.decode(33, ' !@')).toBe(' !@');
-    // });
+    
+     it('should return " !@" para " !@"', () => {
+      expect(cipher.decode(33, ' !@')).toBe(' !@');
+     });
   });
 
 });
